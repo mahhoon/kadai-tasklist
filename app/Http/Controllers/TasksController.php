f70a3ec21389a863f4a7797624b2ce49e6d94b9b
maho.oigawa <maho.oigawa@gmail.com>
2022-07-23 09:03:52 +0000

    fix authcheck

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -96,10 +96,13 @@ class TasksController extends Controller
         ]);
         
         $task = Task::findOrFail($id);
-        $task->content = $request->content;
-        $task->status = $request->status;
-        $task->save();
         
+        if(\Auth::id() === $task->user_id) {
+            $task->content = $request->content;
+            $task->status = $request->status;
+            $task->save();
+        }
+
         return redirect('/');
     }
 
